396f92a470d8fa740edf2726f1d4998c9a8b445a
Isaac Arnold <isaacarnold@icloud.com>
2021-09-28 09:56:47 +1300

    Update gatsby-config.js

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby New Template",
+    title: "Buy Our Stuff",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-styled-components",
+      options: {
+        // ssr: false
+        displayName: true,
+        // minify: false
+      },
+    },
+  ],
 };
