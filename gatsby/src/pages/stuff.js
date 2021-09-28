ff139006dfc930758f64580aee0ed59596708c91
Isaac Arnold <isaacarnold@icloud.com>
2021-09-28 20:03:36 +1300

    Adds stuff page

---
+++ gatsby/src/pages/stuff.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const StuffPage = () => {
+  return (
+    <Layout>
+      <h2>This is our stuff!</h2>
+      {/* Filter */}
+      {/* Products */}
+    </Layout>
+  );
+};
+
+export default StuffPage;
