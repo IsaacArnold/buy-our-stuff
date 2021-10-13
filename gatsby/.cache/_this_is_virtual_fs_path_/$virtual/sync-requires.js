1e45b122ac2eb6def6f4ee8fb8f1002a1ccebd9a
Isaac Arnold <isaacarnold@icloud.com>
2021-10-13 21:32:31 +1300

    Adds 404 page

--- gatsby/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ gatsby/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/IsaacArnold/Documents/web-projects/buy-our-stuff/gatsby/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/IsaacArnold/Documents/web-projects/buy-our-stuff/gatsby/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/IsaacArnold/Documents/web-projects/buy-our-stuff/gatsby/src/pages/index.js")),
   "component---src-pages-stuff-js": preferDefault(require("/Users/IsaacArnold/Documents/web-projects/buy-our-stuff/gatsby/src/pages/stuff.js")),
